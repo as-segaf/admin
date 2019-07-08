1d7511a78731bef1bb62459f638b739e9e42e1b8
as-segaf <ziyanassegaf20@gmail.com>
2019-07-08 09:50:19 +0700

    add data

--- admin/data.php
+++ admin/data.php
@@ -1,9 +1,31 @@
 <?php 
 	include'../config/config.php';
-	$date = $_POST['date'];
-	$agama = mysqli_query($host, "SELECT AVG((ngaji+dhuha+tahajud)/3) WHERE date_survey='$date'");
-	$it    = mysqli_query($host, "SELECT AVG((ngoding+ngetik+belajar)/3) WHERE date_survey='$date'");
-	$kedisiplinan = mysqli_query($host, "SELECT AVG((tidur+bangun+sholat)/3) WHERE date_survey='$date'");
 
+	if (isset($_POST['date'])) {
+	 $date          = $_POST['date'];
+	 $agama		    = mysqli_query($host, "SELECT AVG((ngaji+dhuha+tahajud)/3) AS sum FROM survey WHERE date_survey='$date'");
+	 $it    	    = mysqli_query($host, "SELECT AVG((ngoding+ngetik+belajar)/3) AS sum FROM survey WHERE date_survey='$date'");
+	 $kedisiplinan  = mysqli_query($host, "SELECT AVG((tidur+bangun+sholat)/3) AS sum FROM survey WHERE date_survey='$date'");
+
+	 $data  	= mysqli_fetch_array($agama);
+	 $data 	    = mysqli_fetch_array($it);
+	 $data 	    = mysqli_fetch_array($kedisiplinan);
+	 $dataAgama = number_format($data['sum'],1);
+	 $dataIt    = number_format($data['sum'],1);
+	 $dataDis   = number_format($data['sum'],1);
+		
+	}else{
+		$date = date("Y-m-d");
+		$agama		    = mysqli_query($host, "SELECT AVG((ngaji+dhuha+tahajud)/3) AS sum FROM survey WHERE date_survey='$date'");
+		$it    	    = mysqli_query($host, "SELECT AVG((ngoding+ngetik+belajar)/3) AS sum FROM survey WHERE date_survey='$date'");
+		$kedisiplinan  = mysqli_query($host, "SELECT AVG((tidur+bangun+sholat)/3) AS sum FROM survey WHERE date_survey='$date'");
+
+		$data      = mysqli_fetch_array($agama);
+		$data      = mysqli_fetch_array($it);
+		$data 	   = mysqli_fetch_array($kedisiplinan);
+		$dataAgama = number_format($data['sum'],1);
+		$dataIt    = number_format($data['sum'],1);
+		$dataDis   = number_format($data['sum'],1);
+	}
 
  ?>
